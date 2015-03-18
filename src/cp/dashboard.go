718392dce076fd73909c9181f49321c20bda204f
HJin_me <hjin.me@gmail.com>
2015-03-19 02:22:49 +0800

    for template update

--- src/cp/dashboard.go
+++ src/cp/dashboard.go
@@ -3,5 +3,5 @@ package cp
 import "github.com/hjin-me/banana"
 
 func DashBoard(ctx banana.Context) {
-	ctx.Tpl("cp/dashboard.html", nil)
+	ctx.Tpl("cp/dashboard", nil)
 }
--- src/cp/posts.go
+++ src/cp/posts.go
@@ -3,5 +3,5 @@ package cp
 import "github.com/hjin-me/banana"
 
 func Posts(ctx banana.Context) {
-	ctx.Tpl("cp/posts.html", nil)
+	ctx.Tpl("cp/posts", nil)
 }
