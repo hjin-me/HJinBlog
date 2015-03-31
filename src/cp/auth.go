146b5131adf237d1a4002cf2c90a60a5a200f7d0
HJin.me <hjin.me@gmail.com>
2015-03-31 22:46:47 +0800

    add users

--- src/cp/auth.go
+++ src/cp/auth.go
@@ -1,11 +1,34 @@
 package cp
 
-import "github.com/hjin-me/banana"
+import (
+	"models"
+	"net/http"
+
+	"github.com/hjin-me/banana"
+)
 
 func Auth(ctx banana.Context) {
 	ctx.Req().Cookie("bnuid")
 }
 
-func Login(ctx banana.Context) error {
+func LoginPage(ctx banana.Context) error {
 	return ctx.Tpl("cp:page/login", 0)
+}
+
+func Login(ctx banana.Context) error {
+
+	r := ctx.Req()
+	username, pwd := r.FormValue("username"), r.FormValue("pwd")
+	result, err := models.UserCheck(username, pwd)
+	if err != nil {
+		return err
+	}
+	if result {
+		http.Redirect(ctx.Res(), ctx.Req(), "/cp/dashboard", http.StatusFound)
+	} else {
+		http.Redirect(ctx.Res(), ctx.Req(), "/login?error", http.StatusFound)
+	}
+
+	return nil
+
 }
